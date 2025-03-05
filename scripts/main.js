9bf0432d21d76d23d277af58d1d860cc55f95a38
errorinarrow <kevin30302456@gmail.com>
2025-03-05 11:31:02 +0800

    Finished js script

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,23 @@ function Create_button(){
     cell1.innerHTML = '<td><input type="checkbox"></td>';
     cell2.innerHTML = document.getElementById("taskname").value;
     cell3.innerHTML = '<td><button onclick="Delete_button(this)">delete</button></td>';
+    setTable();
 }
 
 function Delete_button(e){
     var i = e.parentNode.parentNode.rowIndex;
     table.deleteRow(i);
+    setTable();
+}
+
+function setTable(){
+    let mytable = document.getElementById("task table").innerHTML;
+    localStorage.setItem("mytable", mytable);
+}
+
+if(!localStorage.getItem("mytable")){
+    setTable();
+} else {
+    let storedtable = localStorage.getItem("mytable");
+    document.getElementById("task table").innerHTML = storedtable;
 }
