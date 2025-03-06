92792543cfc36fe3786d2343b62f641b50db7c22
errorinarrow <kevin30302456@gmail.com>
2025-03-06 09:48:56 +0800

    Fix checkbox bug

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ function Create_button(){
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
-    cell1.innerHTML = '<td><input type="checkbox"></td>';
+    cell1.innerHTML = '<td><input type="checkbox" onclick="setTable()"></td>';
     cell2.innerHTML = document.getElementById("taskname").value;
     cell3.innerHTML = '<td><button onclick="Delete_button(this)">delete</button></td>';
     setTable();
